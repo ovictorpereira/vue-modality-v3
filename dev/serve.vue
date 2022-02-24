bdc389cbbdc39b019e0ede88d543dcb31aed0d9a
Victor Pereira <victor.smsrio@gmail.com>
2022-02-24 16:19:01 -0300

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+import VueModalityV3 from '@/vue-modality-v3.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    VueModalityV3
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-modality-v3 />
+  </div>
+</template>
